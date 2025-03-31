6e15360d08e2e8d3fc0c8951dabae145fc3e675a
Babyin1251341d <c.vitalii@bitchsson.com>
2025-03-31 22:14:08 +0300

    dfg

--- bot.js
+++ bot.js
@@ -1,17 +1,15 @@
-    const { Telegraf } = require('telegraf');
-    require('dotenv').config();
+const { Telegraf } = require('telegraf');
+require('dotenv').config();
 
-    // Отримання токена з .env
-    const bot = new Telegraf(process.env.BOT_TOKEN);
+// Отримання токена з .env
+const bot = new Telegraf(process.env.BOT_TOKEN);
 
-    // Обробник повідомлень
-    bot.on('message', (ctx) => {
-        console.log(`Chat ID: ${ctx.chat.id}`);
-        ctx.reply(`Ваш chat_id: ${ctx.chat.id}`);
-    });
+// Обробник повідомлень
+bot.on('text', (ctx) => {
+    ctx.reply('Hello!');
+});
 
-    // Запуск бота
-    bot.launch();
+// Запуск бота
+bot.launch();
 
-    console.log('Bot is running...');
-        
+console.log('Bot is running...');
