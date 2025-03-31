c95bc451669e0d6ba3f7d2181952b425515d0a93
Babyin1251341d <c.vitalii@bitchsson.com>
2025-03-31 22:38:34 +0300

    qwe

--- bot.js
+++ bot.js
@@ -13,9 +13,10 @@ bot.on('message', async (ctx) => {
     const messageText = ctx.message.text;
     const chatId = ctx.chat.id;
     const messageId = ctx.message.message_id;
-
+    ctx.reply(messageText,messageId,chatId);
     if (messageText && messageText.toLowerCase().includes("привіт")) {
         try {
+            ctx.reply("видаляю нахуй");
             await ctx.deleteMessage(messageId);
             console.log(`Deleted message in chat ${chatId}: "${messageText}"`);
         } catch (error) {
