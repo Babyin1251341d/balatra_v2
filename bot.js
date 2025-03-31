3a0053ef7f146bea6ea5bdfbdc60c29b1ba70959
Babyin1251341d <c.vitalii@bitchsson.com>
2025-03-31 22:02:10 +0300

    mnb

--- bot.js
+++ bot.js
@@ -5,8 +5,10 @@
     const bot = new Telegraf(process.env.BOT_TOKEN);
 
     // Обробник повідомлень
-    console.log(`Chat ID: ${ctx.chat.id}`);
-    ctx.reply(`Ваш chat_id: ${ctx.chat.id}`);
+    bot.on('message', (ctx) => {
+        console.log(`Chat ID: ${ctx.chat.id}`);
+        ctx.reply(`Ваш chat_id: ${ctx.chat.id}`);
+    });
 
     // Запуск бота
     bot.launch();
