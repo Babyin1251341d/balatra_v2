881cd327d127ce1ed3ccf5d3fc4fce8ed613390b
Babyin1251341d <c.vitalii@bitchsson.com>
2025-03-31 22:41:48 +0300

    qwe

--- bot.js
+++ bot.js
@@ -5,8 +5,20 @@ require('dotenv').config();
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
 // Обробник повідомлень
-bot.on('text', (ctx) => {
-    ctx.reply('Hello naxui zxc!');
+bot.on('text', async (ctx) => {
+    const messageText = ctx.message.text;
+    const chatId = ctx.chat.id;
+    const messageId = ctx.message.message_id;
+    ctx.reply(messageText,messageId,chatId);
+    if (messageText && messageText.toLowerCase().includes("привіт")) {
+        try {
+            ctx.reply("видаляю нахуй");
+            await ctx.deleteMessage(messageId);
+            console.log(`Deleted message in chat ${chatId}: "${messageText}"`);
+        } catch (error) {
+            console.error(`Failed to delete message:`, error);
+        }
+    }
 });
 
 bot.on('message', async (ctx) => {
